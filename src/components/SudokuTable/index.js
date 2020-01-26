7fef5f6d20a1501a59c97fe394f8d099da355cca
Maija <maijaaaw@gmail.com>
2020-01-27 00:05:11 +0200

    Add styles for sudoku table

--- src/components/SudokuTable/index.js
+++ src/components/SudokuTable/index.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 import "./../../styles/Table.css";
+import "./../../styles/App.css";
 
 export default class SudokuTable extends Component {
   render() {
-    let cells = this.props.table.map(cell => {
-      return <span>{cell}</span>;
-    });
-
-    return <div>{cells}</div>;
+    let cells = [];
+    for (let i = 0; i < this.props.table.length; i++) {
+      cells.push(
+        <button key={i} id={i}>
+          {this.props.table[i]}
+        </button>
+      );
+      if ((i + 1) % 9 === 0) {
+        cells.push(<br />);
+      }
+    }
+    return (
+      <div className="sudokuTable">
+        <header className="Table-header">Sudoku:</header>
+        {cells}
+      </div>
+    );
   }
 }
