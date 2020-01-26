c51f3f32dd8e9ad6be699bffc6423601a9d1df55
Maija <maijaaaw@gmail.com>
2020-01-26 16:33:57 +0200

    Add colours and stuff

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+
 import "./styles/App.css";
 
 import SudokuTable from "./components/SudokuTable";
@@ -10,26 +10,51 @@ import solveSudoku from "@mattflow/sudoku-solver";
 export default class App extends Component {
   constructor(props) {
     super(props);
+    let array = [];
+    for (let i = 0; i < 81; i++) {
+      array.push("0");
+    }
+
     this.state = {
-      table: [],
-      number: 0,
-      isOwn: false
+      table: [...array]
     };
+
+    this.onClear = this.onClear.bind(this);
+    this.onCheck = this.onCheck.bind(this);
+  }
+
+  onCheck() {
+    console.log("Checking the table...");
+    //TODO: get string of the hard number values
+    //TODO: solve the sudoku
+    //TODO: compare given numbers to the solution and mark wrong numbers
+    //TODO: update wrongs to state.table (and re-render)
+  }
+
+  onClear() {
+    console.log("Clearing the table...");
+    let array = [];
+    for (let i = 0; i < 81; i++) {
+      array.push("0");
+    }
+    this.setState({ table: [...array] });
   }
 
   render() {
     const s = solveSudoku(
       "123456789456789120000000000000000000000000000000000000000000000000000000000000000"
     );
+    //console.log(s);
 
     return (
       <div className="App">
         <header className="App-header">Sudoku Detector</header>
+        <p>{s}</p>
         <code>
           <pre>{JSON.stringify(s, null, 2)}</pre>
         </code>
-        <Buttons />
-        <SudokuTable />
+        <Buttons onClear={this.onClear} onCheck={this.onCheck} />
+        <SudokuTable table={this.state.table} />
       </div>
     );
   }
--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -2,28 +2,105 @@ import React, { Component } from "react";
 import "./../../styles/Buttons.css";
 
 export default class Buttons extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      number: 0,
+      isOwn: false,
+      activeColor: "lightblue",
+      notActiveColor: "gray"
+    };
+  }
+
+  changeType(isOwn) {
+    console.log("Changing isOwn to", isOwn);
+    this.setState({ isOwn: isOwn });
+  }
+
+  changeNumber(number) {
+    console.log("Changing number to", number);
+    this.setState({ number: number });
+  }
+
+  onClear() {
+    this.setState({ number: 0 });
+    this.props.onClear();
+  }
+
+  onCheck() {
+    this.setState({ number: 0 });
+    this.props.onCheck();
+  }
+
   render() {
+    let numbers = [];
+    for (let i = 1; i < 10; i++) {
+      numbers.push(i);
+    }
+
+    let numberButtons = numbers.map(number => {
+      if (number % 3 === 1) {
+        return (
+          <div>
+            <button
+              className="button"
+              style={
+                number === this.state.number
+                  ? { backgroundColor: this.state.activeColor }
+                  : { backgroundColor: this.state.notActiveColor }
+              }
+              onClick={() => this.changeNumber(number)}
+            >
+              {number}
+            </button>
+            <button
+              className="button"
+              style={
+                number + 1 === this.state.number
+                  ? { backgroundColor: this.state.activeColor }
+                  : { backgroundColor: this.state.notActiveColor }
+              }
+              onClick={() => this.changeNumber(number + 1)}
+            >
+              {number + 1}
+            </button>
+            <button
+              className="button"
+              style={
+                number + 2 === this.state.number
+                  ? { backgroundColor: this.state.activeColor }
+                  : { backgroundColor: this.state.notActiveColor }
+              }
+              onClick={() => this.changeNumber(number + 2)}
+            >
+              {number + 2}
+            </button>
+          </div>
+        );
+      } else {
+        return;
+      }
+    });
+
     return (
       <div className="Buttons">
         <div>
-          <button>Hard number</button>
-          <button>My number</button>
+          <button className="typeButton" onClick={() => this.changeType(false)}>
+            Hard number
+          </button>
+          <button className="typeButton" onClick={() => this.changeType(true)}>
+            My number
+          </button>
         </div>
         <div>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <br />
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <br />
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <br />
-          <button>clear</button>
-          <button>check</button>
+          {numberButtons}
+          <button className="clearButton" onClick={() => this.onClear()}>
+            clear
+          </button>
+          <button className="checkButton" onClick={() => this.onCheck()}>
+            check
+          </button>
         </div>
       </div>
     );
