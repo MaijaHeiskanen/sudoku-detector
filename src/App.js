3998f0b70aea8712adb3af8b07f2bde54ca8730f
Maija <maijaaaw@gmail.com>
2020-01-26 14:55:11 +0200

    Add stuff

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./styles/App.css";
 
 import SudokuTable from "./components/SudokuTable";
 import Buttons from "./components/Buttons";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">Sudoku Detector</header>
-      <Buttons />
-      <SudokuTable />
-    </div>
-  );
+import solveSudoku from "@mattflow/sudoku-solver";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      table: [],
+      number: 0,
+      isOwn: false
+    };
+  }
+
+  render() {
+    const s = solveSudoku(
+      "123456789456789120000000000000000000000000000000000000000000000000000000000000000"
+    );
+
+    return (
+      <div className="App">
+        <header className="App-header">Sudoku Detector</header>
+        <code>
+          <pre>{JSON.stringify(s, null, 2)}</pre>
+        </code>
+        <Buttons />
+        <SudokuTable />
+      </div>
+    );
+  }
 }
 
-export default App;
+console.log(this);
